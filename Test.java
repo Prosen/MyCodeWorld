82c2a4197a14349f8cbf835a7007e0925a6472ba
= <=>
2015-03-04 03:33:41 +0600

    next Hello world

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test {
 	
 	public static void main( String args[] ) {
-		System.out.println("Helllo");
 		System.out.println("Helllo World");
+		System.out.println("Next Helllo World");
+		System.out.println("Next Next Helllo World");
 	}
 }
